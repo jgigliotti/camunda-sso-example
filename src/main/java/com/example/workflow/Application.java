c5d276777b2a54e15473f76fbd35446ac5d4f9ff
Justin Gigliotti <jgigliotti@bp-3.com>
2020-12-04 11:58:51 -0600

    Add spring security and github oauth

--- src/main/java/com/example/workflow/Application.java
+++ src/main/java/com/example/workflow/Application.java
@@ -2,8 +2,10 @@ package com.example.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class Application {
 
   public static void main(String[] args) {
